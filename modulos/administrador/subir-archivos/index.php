3b065787c9fd414eedd03d0647b873ebe4fe3ebf
Esteban Vintimilla <esteban.vintimilla@gmail.com>
2015-07-29 22:07:09 -0500

    combo subir archivo
    combo subir archivo

--- modulos/administrador/subir-archivos/index.php
+++ modulos/administrador/subir-archivos/index.php
@@ -21,6 +21,13 @@
 			<div class="page-header"><h3>SUBIR ARCHIVO</h3></div>
             <form class="form-horizontal" method="post" action="proveedor.php" enctype="multipart/form-data" id="subirArchivo">
             <div class="row">
+            	<select name="mi_combobox"> 
+				<option value="proveedor.php">Cliente</option> 
+				<option value="proveedor.php">Proveedor</option> 
+                <option value="producto.php">Producto</option>
+				</select> 
+            </div>
+            <div class="row">
                  Archivo
                  <input name="archivo" type="file" id="archivo">
             </div>
